1164b7fe8d9f2721fc37ea794c2568a791aef3a2
ravi990088 <ravi@adshi5.com>
2025-04-24 12:03:53 +0530

    first commit

--- src/utils/slices/authSlice.js
+++ src/utils/slices/authSlice.js
@@ -1,52 +1,58 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const organizationUser = localStorage.getItem("isOrganizationUser");
-const stores = localStorage.getItem('stores');
+const stores = localStorage.getItem("stores");
 
 const initialState = {
-    initialAccessToken: localStorage.getItem('initialAccessToken'),
-    accessToken: localStorage.getItem('accessToken') || null,
-    refreshToken: localStorage.getItem('refreshToken') || null,
-    isOrganizationUser : organizationUser != "undefined" && JSON.parse(organizationUser),
-    stores : (stores != "undefined" && JSON.parse(stores)) || [],
-    user: null,
+  initialAccessToken: localStorage.getItem("initialAccessToken"),
+  accessToken: localStorage.getItem("accessToken") || null,
+  refreshToken: localStorage.getItem("refreshToken") || null,
+  isOrganizationUser:
+    organizationUser != "undefined" && JSON.parse(organizationUser),
+  stores: (stores != "undefined" && JSON.parse(stores)) || [],
+  user: null,
 };
 
 const authSlice = createSlice({
-    name: 'auth',
-    initialState,
-    reducers: {
-        setTokens: (state, action) => {
-            const { token, refreshToken, isOrganizationUser, stores } = action.payload;
-            state.accessToken = token;
-            state.refreshToken = refreshToken;
-            state.isOrganizationUser = isOrganizationUser;
-            state.stores = stores;
-            localStorage.setItem('initialAccessToken', accessToken);
-            localStorage.setItem('refreshToken', refreshToken);
-            localStorage.setItem('isOrganizationUser', JSON.stringify(isOrganizationUser));
-            localStorage.setItem('stores', JSON.stringify(stores));
-        },
+  name: "auth",
+  initialState,
+  reducers: {
+    setTokens: (state, action) => {
+      const { token, refreshToken, isOrganizationUser, stores } =
+        action.payload;
+      state.accessToken = token;
+      state.refreshToken = refreshToken;
+      state.isOrganizationUser = isOrganizationUser;
+      state.stores = stores;
+      localStorage.setItem("initialAccessToken", accessToken);
+      localStorage.setItem("refreshToken", refreshToken);
+      localStorage.setItem(
+        "isOrganizationUser",
+        JSON.stringify(isOrganizationUser)
+      );
+      localStorage.setItem("stores", JSON.stringify(stores));
+    },
 
-        setUser: (state, action) => {            
-            state.user = action.payload;
-        },
+    setUser: (state, action) => {
+      state.user = action.payload;
+    },
 
-        setOrganizationTokens: (state, action) => {
-            state.accessToken = action.payload.accessToken;
-            state.refreshToken = action.payload.refreshToken;
-            localStorage.setItem('accessToken', action.payload.accessToken);
-            localStorage.setItem('refreshToken', action.payload.refreshToken);
-        },
+    setOrganizationTokens: (state, action) => {
+      state.accessToken = action.payload.accessToken;
+      state.refreshToken = action.payload.refreshToken;
+      localStorage.setItem("accessToken", action.payload.accessToken);
+      localStorage.setItem("refreshToken", action.payload.refreshToken);
+    },
 
-        logout: (state) => {
-            state.accessToken = null;
-            state.refreshToken = null;
-            localStorage.removeItem("accessToken");
-            localStorage.removeItem("refreshToken");
-        }
-    }
+    logout: (state) => {
+      state.accessToken = null;
+      state.refreshToken = null;
+      localStorage.removeItem("accessToken");
+      localStorage.removeItem("refreshToken");
+    },
+  },
 });
 
-export const { setTokens, setUser, setOrganizationTokens, logout } = authSlice.actions;
+export const { setTokens, setUser, setOrganizationTokens, logout } =
+  authSlice.actions;
 export default authSlice.reducer;
